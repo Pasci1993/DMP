1f5232aaf7d64f101a6aa8cc2a1d02cbb1be277a
Pasci1993 <kamaeric22@gmail.com>
2025-08-21 19:44:01 +0000

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,213 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MonAppli());
+}
+
+class MonAppli extends StatelessWidget {
+  const MonAppli({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mon Magazine',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: const PageAccueil(title: 'Mon Magazine'),
+    );
+  }
+}
+
+class PageAccueil extends StatefulWidget {
+  const PageAccueil({super.key, required this.title});
+
+  final String title;
+
+  @override
+  State<PageAccueil> createState() => _PageAccueil();
+}
+
+class _PageAccueil extends State<PageAccueil> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        shape: const RoundedRectangleBorder(
+          borderRadius: BorderRadius.only(
+            topLeft: Radius.circular(25),
+            topRight: Radius.circular(25),
+          ),
+        ),
+        centerTitle: true,
+        backgroundColor: const Color.fromARGB(255, 255, 100, 139),
+        title: Text("Magazine Infos", style: TextStyle(color: Colors.white)),
+        leading: IconButton(
+          icon: const Icon(Icons.menu, color: Colors.white),
+          onPressed: () {},
+        ),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.search, color: Colors.white),
+            onPressed: () {},
+          ),
+        ],
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image(image: AssetImage("assets/images/magazineInfo.jpg")),
+            PartieTitre(),
+            PatrtieTexte(),
+            PartieIcone(),
+            PartieRubrique(),
+          ],
+        ),
+      ),
+
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Color.fromARGB(255, 255, 100, 139),
+
+        tooltip: 'Press the button',
+        onPressed: () {
+          ScaffoldMessenger.of(context).showSnackBar(
+            const SnackBar(content: Text("Vous avez cliqué dessus")),
+          );
+        },
+        child: const Text("Click"),
+      ),
+    );
+  }
+
+  //child: const Text("Click"),
+  // ), // This trailing comma makes auto-formatting nicer for build methods.
+  //);
+}
+
+//}
+class PartieTitre extends StatelessWidget {
+  const PartieTitre({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      padding: const EdgeInsets.all(20),
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Text(
+            "Bienvenue au Magazine Info",
+            style: TextStyle(fontSize: 25, fontWeight: FontWeight.w800),
+          ),
+          Text(
+            "Votre magazine numerique,votre source d'inspiration ",
+
+            style: TextStyle(fontSize: 14, fontWeight: FontWeight.w500),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PatrtieTexte extends StatelessWidget {
+  const PatrtieTexte({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: const Text(
+        "Magazine Infos est bien plus qu’un simple magazine d’informations. C’est une passerelle vers le monde d’informations, une source inestimable de connaissances et d’actualités soigneusement sélectionnées pour vous éclairer sur les enjeux mondiaux, la culture, la science, la sante et voir même le divertissement (les jeux). ",
+
+        style: TextStyle(fontSize: 15, fontWeight: FontWeight.w800),
+      ),
+    );
+  }
+}
+
+class PartieIcone extends StatelessWidget {
+  const PartieIcone({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const Icon(Icons.phone, color: Colors.pink),
+                const SizedBox(height: 5),
+                Text(
+                  "Tel".toUpperCase(),
+                  style: const TextStyle(color: Colors.pink),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const Icon(Icons.phone, color: Colors.pink),
+                const SizedBox(height: 5),
+                Text(
+                  "Mail".toUpperCase(),
+                  style: const TextStyle(color: Colors.pink),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                const Icon(Icons.phone, color: Colors.pink),
+                const SizedBox(height: 5),
+                Text(
+                  "Partage".toUpperCase(),
+                  style: const TextStyle(color: Colors.pink),
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class PartieRubrique extends StatelessWidget {
+  const PartieRubrique({super.key});
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: const EdgeInsets.symmetric(horizontal: 20),
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(15),
+            child: const Image(
+              image: AssetImage("assets/images/un.png"),
+              width: 150,
+              height: 100,
+            ),
+          ),
+          ClipRRect(
+            borderRadius: BorderRadiusGeometry.circular(15),
+            child: const Image(
+              image: AssetImage("assets/images/deux.png"),
+              width: 150,
+              height: 100,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
